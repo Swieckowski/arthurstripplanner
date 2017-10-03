006592fc8cebe47483d5bdb601b9498dcd4bd3af
Swieckowski <a.swieckowski@gmail.com>
2017-10-03 17:23:30 -0400

    all by arthur all good

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const mapboxgl = require("mapbox-gl");
+const markerFactory = require('./marker.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXZpbGxhcnJlYWwwMSIsImEiOiJjajhicnJ4ZXUwMWIyMnFybG8ydmk1ajdpIn0.zFonWc7d_835Jqa1S2_RUg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = markerFactory('hotel', -74.009151, 40.705086);
+marker.addTo(map);
